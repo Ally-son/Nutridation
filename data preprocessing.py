1cbc266d5686a22a60e6005673fa682c9860f0b8
tohmato <an0001oh@e.ntu.edu.sg>
2025-04-14 18:24:41 +0800

    shows changes in size of data set

--- data preprocessing.py
+++ data preprocessing.py
@@ -16,17 +16,14 @@ data_copy = data_copy[columns]
 # Count how many foods are in each category
 category_counts = data_copy['RecipeCategory'].value_counts()
 print("Category counts:\n", category_counts)
+dataset_size = data_copy.shape
+print(f"\nSize of the dataset: {dataset_size[0]} rows and {dataset_size[1]} columns")
+
 
 # Since there are 311 unique categories, with some only having 1 recipie, we will remove categories that have less than 100 recipies
 threshold = 100
 low_freq_categories = category_counts[category_counts < threshold].index
 data_copy = data_copy[~data_copy['RecipeCategory'].isin(low_freq_categories)]
-
-# Check for remaining categories
-category_counts = data_copy['RecipeCategory'].value_counts()
-print("\nCategory counts after filtering:\n", category_counts)
-dataset_size = data_copy.shape
-print(f"\nSize of the dataset after filtering: {dataset_size[0]} rows and {dataset_size[1]} columns")
 
 
 # List of unwanted categories
@@ -34,10 +31,12 @@ unwanted_categories = ['Dessert','Frozen Desserts','Sauces', 'Condiments', 'Hous
                        'Cheesecake', 'Bar Cookie', 'Jellies' ,'Candy', 'Bath/Beauty', 
                        'Homeopathy/Remedies', 'Ice Cream', 'Chocolate Chip Cookies']
 
+
 # Remove unwanted categories
 filtered_data = data_copy[~data_copy['RecipeCategory'].isin(unwanted_categories)]
 category_counts = filtered_data['RecipeCategory'].value_counts()
 print("\nCategory counts after filtering:\n", category_counts)
+
 
 # Deleting duplicates and rows with missing values
 filtered_data = filtered_data.dropna()  
@@ -47,7 +46,8 @@ filtered_data = filtered_data.drop_duplicates()
 
 
 dataset_size = filtered_data.shape
-print(f"Size of the dataset after filtering: {dataset_size[0]} rows and {dataset_size[1]} columns")
+print(f"\nSize of the dataset after filtering: {dataset_size[0]} rows and {dataset_size[1]} columns")
+
 
 # Standardising the data
 from sklearn.preprocessing import StandardScaler
